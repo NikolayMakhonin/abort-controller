df9b1e895f731922ec9a7e1412047832596d97b8
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2022-05-21 10:36:11 +0200

    develop: added karma tests. All Tests OK.

--- src/abort-controller/EventTargetImpl.ts
+++ src/abort-controller/EventTargetImpl.ts
@@ -1,4 +1,5 @@
 import {IEventTarget} from './contracts'
+import {initClass} from './helpers'
 
 let _EventTarget: { new(): IEventTarget; prototype: IEventTarget }
 
@@ -6,7 +7,7 @@ if (typeof window !== 'undefined') {
   _EventTarget = function EventTarget() {
     return document.createDocumentFragment()
   } as any
-  Object.setPrototypeOf(_EventTarget, DocumentFragment.prototype)
+  initClass(_EventTarget, DocumentFragment)
 }
 
 export { _EventTarget as EventTargetImpl}
