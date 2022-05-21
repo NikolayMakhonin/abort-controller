fbcec28faa49f600b6120fa8d1912ee21eb2fb47
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2022-05-21 15:28:06 +0200

    develop: CI karma tests...

--- nyc.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	include     : ['src/**/*.ts'],
-	exclude     : ['**/test/**', '**/*.test.*', 'dist/browser.js'],
-	reporter    : ['json'],
-	'temp-dir'  : `./tmp/coverage/tmp`,
-	'report-dir': `./tmp/coverage/mocha/json`,
-}
